a33bff622ce681265a0bf21878f523c59c982ac3
Kieran Simpson <kierans777@gmail.com>
2021-10-14 21:28:20 +1100

    Add getters for headers.

--- src/web/lambda/headers.js
+++ src/web/lambda/headers.js
@@ -1,12 +1,85 @@
 "use strict";
 
+const First = require("crocks/First");
+const Result = require("crocks/Result");
+
 const assign = require("crocks/helpers/assign");
+const compose = require("crocks/helpers/compose");
+const curry = require("crocks/helpers/curry");
+const either = require("crocks/pointfree/either");
+const foldMap = require("crocks/pointfree/foldMap");
+const getProp = require("crocks/Maybe/getProp");
+const map = require("crocks/pointfree/map");
+const option = require("crocks/pointfree/option");
+const pipe = require("crocks/helpers/pipe");
 const reduce = require("crocks/pointfree/reduce");
+const valueOf = require("crocks/pointfree/valueOf");
+
+const { applyFunctor } = require("@epistemology-factory/crocks-ext/helpers");
+const {
+	capitalise,
+	join,
+	lowerCase,
+	split
+} = require("@epistemology-factory/crocks-ext/String");
 
 const defaultCorsHeaders = {
 	"Access-Control-Allow-Origin": "*",
 	"Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent"
 }
+
+// joinDash :: [ String ] -> String
+const joinDash = join("-")
+
+// splitDash :: String -> [ String ]
+const splitDash = split("-")
+
+// changeCase :: (String -> String) -> String -> String
+const changeCase = (fn) => compose(joinDash, map(fn), splitDash)
+
+// lowercased :: String -> String
+const lowercased = changeCase(lowerCase)
+
+// capitalised :: String -> String
+const capitalised = changeCase(capitalise)
+
+// getHeaderVariation :: (String -> String) -> String -> a -> Maybe b
+const getHeaderVariation = (fn) => compose(getProp, fn)
+
+// variations :: [ (String -> a -> Maybe b) ]
+const variations = [
+	getHeaderVariation(lowercased),
+	getHeaderVariation(capitalised)
+]
+
+/*
+ * `getHeader` looks for a header in an object, but will check for the presence of the key
+ * taking into account variations in how headers are capitalised.
+ *
+ * Will check for headers
+ * - lowercased eg: 'origin' or 'content-type'
+ * - capitalised eg: 'Origin' or 'Content-Type'
+ */
+// getHeader :: String -> a -> Maybe b
+const getHeader = curry((header) =>
+	pipe(
+		applyFunctor(applyFunctor(variations, header)),
+		compose(valueOf, foldMap(First))
+	)
+)
+
+const getHeaderOr = curry((value, header) =>
+	compose(option(value), getHeader(header))
+)
+
+/*
+ * An error function will be called with the header name if the header is not found in any
+ * variation.
+ */
+// getHeaderOrErr :: (String -> a) -> String -> b -> Result a c
+const getHeaderOrErr = curry((error, header) =>
+	compose(either(() => Result.Err(error(header)), Result.Ok), getHeader(header))
+)
 
 /**
  * Merges header objects together. The first value for a given key is preserved, so
@@ -17,5 +90,8 @@ const mergeHeaders = (...headers) => reduce(assign, {}, headers)
 
 module.exports = {
 	defaultCorsHeaders,
+	getHeader,
+	getHeaderOr,
+	getHeaderOrErr,
 	mergeHeaders
 }
--- test/web/lambda/headers.test.js
+++ test/web/lambda/headers.test.js
@@ -1,8 +1,16 @@
 "use strict";
 
-const { assertThat, is } = require("hamjest");
+const { identity } = require("crocks");
 
-const { mergeHeaders } = require("../../../src/web/lambda/headers");
+const { allOf, assertThat, equalTo, hasProperty, instanceOf, is, throws } = require("hamjest");
+
+const {
+	getHeader,
+	getHeaderOr,
+	getHeaderOrErr,
+	mergeHeaders
+} = require("../../../src/web/lambda/headers");
+const { throwContents } = require("@epistemology-factory/crocks-ext/utils");
 
 describe("headers", function() {
 	describe("mergeHeaders", function() {
@@ -13,5 +21,57 @@ describe("headers", function() {
 		it("should preserve first header given", function() {
 			assertThat(mergeHeaders({ a: 1}, { a: 2 }), is({ a: 1 }));
 		});
+	});
+
+	describe("getHeader", function() {
+		const headers = {
+			"origin": "http://localhost",
+			"content-type": "text/plain"
+		}
+
+		it("should find header lowercased", function() {
+			assertThat(get("origin"), is("http://localhost"));
+			assertThat(get("content-type"), is("text/plain"));
+		});
+
+		it("should find header capitalized", function() {
+			assertThat(get("Origin"), is("http://localhost"));
+			assertThat(get("Content-Type"), is("text/plain"));
+		});
+
+		it("should return nothing when header not found", function() {
+			assertThat(get("foo"), is(null));
+		});
+
+		function get(header) {
+			return getHeader(header, headers).option(null)
+		}
+	});
+
+	describe("getHeaderOr", function() {
+		const defaultValue = "default";
+
+		it("should return default if header not found", function() {
+			assertThat(get("origin"), is(defaultValue));
+		});
+
+		function get(header) {
+			return getHeaderOr(defaultValue, header, {})
+		}
+	});
+
+	describe("getHeaderOrErr", function() {
+		const missingHeader = (header) => new Error(`Missing ${header}`);
+
+		it("should return error when header not found", function() {
+			assertThat(() => get("origin"), throws(allOf(
+				instanceOf(Error),
+				hasProperty("message", equalTo("Missing origin"))
+			)))
+		})
+
+		function get(header) {
+			return getHeaderOrErr(missingHeader, header, {}).either(throwContents, identity);
+		}
 	});
 });
