288ff4680d491db1177f9be85bd411f52d08c923
Kieran Simpson <kierans777@gmail.com>
2021-10-18 21:24:30 +1100

    Add function to combine validators.

--- src/validation/validators/validator.js
+++ src/validation/validators/validator.js
@@ -2,9 +2,16 @@
 
 const Result = require("crocks/Result");
 
+const chain = require("crocks/pointfree/chain");
 const compose = require("crocks/helpers/compose");
+const contramap = require("crocks/pointfree/contramap");
 const curry = require("crocks/helpers/curry");
 const ifElse = require("crocks/logic/ifElse");
+const flip = require("crocks/combinators/flip");
+const pipe = require("crocks/helpers/pipe");
+const reduce = require("crocks/pointfree/reduce");
+
+const { applyFunctor } = require("@epistemology-factory/crocks-ext/helpers");
 
 // makeValidator :: (a -> Boolean) -> ([ String ] -> a -> ValidationFailure) -> [ String ] -> a -> Result ValidationFailure a
 const makeValidator = curry((pred, err) => (path) =>
@@ -15,6 +22,15 @@ const makeValidator = curry((pred, err) => (path) =>
 	)
 )
 
+// validators :: Foldable f => f -> [ String ] -> a -> Result ValidationFailure a
+const validators = (...validators) =>
+	pipe(
+		applyFunctor(validators),
+		flip(reduce(flip(chain))),
+		contramap(Result.Ok)
+	)
+
 module.exports = {
-	makeValidator
+	makeValidator,
+	validators
 }
---
+++ test/validation/validators/validator.test.js
@@ -0,0 +1,53 @@
+"use strict";
+
+const { identity } = require("crocks");
+
+const { assertThat, is } = require("hamjest");
+
+const { isDefined } = require("../../../src/validation/validators/common");
+const { isString } = require("../../../src/validation/validators/strings");
+const { validators } = require("../../../src/validation/validators/validator");
+
+const { throwContents, throwResult } = require("@epistemology-factory/crocks-ext/utils");
+const { aValidationFailure } =
+	require("../../../src/test/hamjest/lambda/matchers/validation-failure");
+const { CONSTRAINTS, DEFAULT_MESSAGES } = require("../../../src/validation/validators");
+
+describe("validator", function() {
+	describe("validators", function() {
+		const path = [ "a" ];
+		const validator = validators(isDefined, isString)
+
+		it("should return error when value is undefined", function() {
+			const value = undefined;
+			const result = validator(path)(value).either(identity, throwContents);
+
+			assertThat(result, is(aValidationFailure(
+				path,
+				CONSTRAINTS.IS_DEFINED,
+				DEFAULT_MESSAGES[CONSTRAINTS.IS_DEFINED],
+				value
+			)))
+		});
+
+		it("should return error when value is not string", function() {
+			const value = 123;
+			const result = validator(path)(value).either(identity, throwContents);
+
+			assertThat(result, is(aValidationFailure(
+				path,
+				CONSTRAINTS.IS_STRING,
+				DEFAULT_MESSAGES[CONSTRAINTS.IS_STRING],
+				value
+			)))
+		});
+
+		it("should return value", function() {
+			const value = "foo";
+
+			const result = validator(path)(value).either(throwResult, identity);
+
+			assertThat(result, is(value));
+		})
+	});
+});
