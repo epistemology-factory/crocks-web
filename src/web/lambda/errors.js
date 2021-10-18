841d6d25a70bab7a9285025719fdbfd1e21ddada
Kieran Simpson <kierans777@gmail.com>
2021-10-18 16:05:21 +1100

    Add errors for invalid env vars.

--- src/web/lambda/errors.js
+++ src/web/lambda/errors.js
@@ -1,6 +1,12 @@
 "use strict";
 
+const objOf = require("crocks/helpers/objOf");
+const pipe = require("crocks/helpers/pipe");
+const setProp = require("crocks/helpers/setProp");
+
 const ERROR_TYPES = {
+	INVALID_ENV_VAR: "invalid-env-var",
+	MISSING_ENV_VAR: "missing-env-var",
 	VALIDATION_ERROR: "validation-error"
 }
 
@@ -10,7 +16,22 @@ const validationError = (failures) => ({
 	failures
 })
 
+// envVarError :: String -> String -> Object
+const envVarError = (type) =>
+	pipe(
+		objOf("name"),
+		setProp("type", type)
+	)
+
+// invalidEnvVar :: String -> Object
+const invalidEnvVar = envVarError(ERROR_TYPES.INVALID_ENV_VAR)
+
+// missingEnvVar :: String -> Object
+const missingEnvVar = envVarError(ERROR_TYPES.MISSING_ENV_VAR)
+
 module.exports = {
 	ERROR_TYPES,
+	invalidEnvVar,
+	missingEnvVar,
 	validationError
 }
