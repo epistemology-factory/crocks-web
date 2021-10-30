dbb550cb0832d03ef76ab51a1340d9b3c7c082b1
Kieran Simpson <kierans777@gmail.com>
2021-10-30 11:51:26 +1100

    Add common validation failure matchers.

--- src/test/hamjest/lambda/matchers/validation-failure.js
+++ src/test/hamjest/lambda/matchers/validation-failure.js
@@ -2,6 +2,8 @@
 
 const { allOf, hasProperty, equalTo } = require("hamjest");
 
+const { CONSTRAINTS, DEFAULT_MESSAGES } = require("../../../../validation/validators");
+
 const aValidationFailure = (path, constraint, message, value) =>
 	allOf(
 		hasProperty("path", equalTo(path)),
@@ -9,6 +11,36 @@ const aValidationFailure = (path, constraint, message, value) =>
 		hasProperty("value", equalTo(value))
 	)
 
+const isDefinedFailure = (path) => {
+	return aValidationFailure(
+		path,
+		CONSTRAINTS.IS_DEFINED,
+		DEFAULT_MESSAGES[CONSTRAINTS.IS_DEFINED],
+		undefined
+	);
+}
+
+const isObjectFailure = (path, value) => {
+	return aValidationFailure(
+		path,
+		CONSTRAINTS.IS_OBJECT,
+		DEFAULT_MESSAGES[CONSTRAINTS.IS_OBJECT],
+		value
+	);
+}
+
+const isStringFailure = (path, value) => {
+	return aValidationFailure(
+		path,
+		CONSTRAINTS.IS_STRING,
+		DEFAULT_MESSAGES[CONSTRAINTS.IS_STRING],
+		value
+	);
+}
+
 module.exports = {
-	aValidationFailure
+	aValidationFailure,
+	isDefinedFailure,
+	isObjectFailure,
+	isStringFailure
 }
